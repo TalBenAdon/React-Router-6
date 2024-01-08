9a5f47073c26071546b8ded274a49950cc456807
Tal Ben Adon <talbenadon2@gmail.com>
2024-01-08 20:58:47 +0200

    changed index

--- src/components/layout/AdminLayout/index.jsx
+++ src/components/layout/AdminLayout/index.jsx
@@ -7,7 +7,6 @@ function AdminLayout() {
     <div className={styles.container}>
       <AdminNav />
       <main>
-        {/* Add - the ***** to add the admin pages here */}
         <Outlet />
       </main>
     </div>
